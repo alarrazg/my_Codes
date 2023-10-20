0dc44b4a0a537feb8c35b416b4ee3b067f78342f
alarrazg <alarrazg@gmail.com>
2023-10-20 21:31:08 +0200

    Contador de Clicks: dos contadores y un sumatorio

--- contadorclicks/src/App.js
+++ contadorclicks/src/App.js
@@ -5,33 +5,40 @@ import { useState } from 'react';
 
 function App() {
 
-  const [numClicks, setNumClicks] = useState(0);
+  const [numClicks1, setNumClicks1] = useState(0);
+  const [numClicks2, setNumClicks2] = useState(0);
 
-  const handleClick = () => {
-    setNumClicks(numClicks + 1);
+  const handleClick1 = (nClick) => {
+    setNumClicks1(numClicks1 + 1);
   }
 
+  const handleClick2 = (nclick) => {
+    setNumClicks2(numClicks2 + 1);
+  }
+      
+
   const resetCounter = () => {
-    setNumClicks(0);
+    setNumClicks1(0);
+    setNumClicks2(0);
   }
 
   return (
     <div className='App'>
       <div className='contenedor-principal'>
         <div className='contenedor-counter'>
-          <Counter numClicks={numClicks} />
-          <Counter numClicks={numClicks} />
-          <Counter numClicks={0} />
+          <Counter numClicks={numClicks1} />
+          <Counter numClicks={numClicks2} />
+          <Counter numClicks={numClicks1 + numClicks2} />
         </div>
         <div className='contenedor-button'>
           <Button
             text='Click'
             isButtonClick={true}
-            handleClick={handleClick_1} />
+            handleClick={handleClick1} />
           <Button
             text='Click'
             isButtonClick={true}
-            handleClick={handleClick_2} />
+            handleClick={handleClick2} />
           <Button
             text='Reiniciar'
             isButtonClick={false}
